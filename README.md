b9c79e028e76b5ffa7f00b910111ced56e003e13
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-07-27 17:06:48 +0900

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # esp-idf-websocket-camera
-Take a picture and Publish it via WebSocket.
+Take a picture and Publish it via WebSocket.   
+This project use [ESP32 Camera Driver](https://github.com/espressif/esp32-camera).   
+
+# Hardware requirements
+ESP32 development board with OV2640 camera.   
+If you use other camera, edit sdkconfig.default.   
+![ESP32-Camera-board](https://user-images.githubusercontent.com/6020549/198520670-27ea9bd1-71d8-438f-be58-8516154be4af.JPG)
+
+
+# Software requirements
+esp-idf v4.4/v5.X.   
+
+# Installation
+For AiThinker ESP32-CAM, You have to use a USB-TTL converter.   
+
+|ESP-32|USB-TTL|
+|:-:|:-:|
+|U0TXD|RXD|
+|U0RXD|TXD|
+|GPIO0|GND|
+|5V|5V|
+|GND|GND|
+
+
+```
+git clone https://github.com/nopnop2002/esp-idf-websocket-camera
+cd esp-idf-websocket-camera
+git clone https://github.com/espressif/esp32-camera components/esp32-camera
+idf.py set-target esp32
+idf.py menuconfig
+idf.py flash monitor
+```
+
+# Start firmware
+For AiThinker ESP32-CAM, Change GPIO0 to open and press the RESET button.
+
+# Configuration
+Set the following items using menuconfig.
+
+![config-top](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/20901930-dfc9-45e2-86bf-674ee352214c)
+![config-app](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/5e6a4cf8-ac2c-47a5-a2c0-59dc42a15fde)
+
+## Wifi Setting
+
+![config-wifi-1](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/4c23bb09-8d84-4c40-8bda-648ddc5c810f)
+
+You can connect using the mDNS hostname instead of the IP address.   
+![config-wifi-2](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/faa17c9c-548b-4c81-8202-da74d9772bc8)
+
+You can use static IP.   
+![config-wifi-3](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/c44f1e38-99ec-4de9-9792-a56314700021)
+
+## Using mDNS hostname
+You can connect using the mDNS hostname instead of the IP address.   
+- esp-idf V4.4  
+ If you set CONFIG_MDNS_STRICT_MODE = y in sdkconfig.defaults, the firmware will be built with MDNS_STRICT_MODE.   
+ __If MDNS_STRICT_MODE is not set, mDNS name resolution will not be possible after long-term operation.__   
+- esp-idf V4.4.1   
+ mDNS component has been updated.   
+ If you set CONFIG_MDNS_STRICT_MODE = y in sdkconfig.defaults, the firmware will be built with MDNS_STRICT_MODE.   
+ __Even if MDNS_STRICT_MODE is set, mDNS name resolution will not be possible after long-term operation.__   
+- esp-idf V5.0 or later   
+ mDNS component has been updated.   
+ Long-term operation is possible without setting MDNS_STRICT_MODE.   
+ The following lines in sdkconfig.defaults should be removed before menuconfig.   
+ ```CONFIG_MDNS_STRICT_MODE=y```
+
+## Select Board
+![config-board](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/4b4111b5-d03b-4574-a00a-83e3df248495)
+
+
+
+## Flash Light
+
+ESP32-CAM by AI-Thinker has a flash light on GPIO4.
+
+![config-light](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/3acabf5a-2c4d-4855-9799-4a17257388fd)
+
+
+# Take pictures using Browser
+Click index.html.   
+![browser-1](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/2e73c585-a1d4-42a2-a158-38cf424169f9)
+
+Push connect server button.
+![browser-2](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/ec4e671f-3cd0-4bcc-a4aa-184fe669d238)
+
+Push take picture button.
+![browser-3](https://github.com/nopnop2002/esp-idf-websocket-camera/assets/6020549/7fbca7e9-bc30-40b4-9ad2-5091ed6c88fd)
+
+
---
+++ main/main.c
@@ -0,0 +1,609 @@
+/*
+	Take a picture and Publish it via Web Socket.
+
+	This code is in the Public Domain (or CC0 licensed, at your option.)
+
+	Unless required by applicable law or agreed to in writing, this
+	software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+	CONDITIONS OF ANY KIND, either express or implied.
+
+	I based from here:
+	https://github.com/espressif/esp-idf/tree/master/examples/protocols/http_server/ws_echo_server
+*/
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <mbedtls/base64.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+//#include "esp_sntp.h"
+#include "mdns.h"
+#include "lwip/dns.h"
+
+#include "esp_camera.h"
+#include "camera_pin.h"
+
+#if 0
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+#include "esp_netif.h"
+#include "esp_eth.h"
+#include "protocol_examples_common.h"
+#endif
+
+#include "esp_http_server.h"
+
+/* A simple example that demonstrates using websocket echo server
+ */
+static const char *TAG = "WS_SERVER";
+
+/*
+ * Structure holding server handle
+ * and internal socket fd in order
+ * to use out of request send
+ */
+#if 0
+struct async_resp_arg {
+	httpd_handle_t hd;
+	int fd;
+};
+#endif
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static int s_retry_num = 0;
+
+//static camera_config_t camera_config = {
+camera_config_t camera_config = {
+	.pin_pwdn = CAM_PIN_PWDN,
+	.pin_reset = CAM_PIN_RESET,
+	.pin_xclk = CAM_PIN_XCLK,
+	.pin_sscb_sda = CAM_PIN_SIOD,
+	.pin_sscb_scl = CAM_PIN_SIOC,
+
+	.pin_d7 = CAM_PIN_D7,
+	.pin_d6 = CAM_PIN_D6,
+	.pin_d5 = CAM_PIN_D5,
+	.pin_d4 = CAM_PIN_D4,
+	.pin_d3 = CAM_PIN_D3,
+	.pin_d2 = CAM_PIN_D2,
+	.pin_d1 = CAM_PIN_D1,
+	.pin_d0 = CAM_PIN_D0,
+	.pin_vsync = CAM_PIN_VSYNC,
+	.pin_href = CAM_PIN_HREF,
+	.pin_pclk = CAM_PIN_PCLK,
+
+	//XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+	.xclk_freq_hz = 20000000,
+	.ledc_timer = LEDC_TIMER_0,
+	.ledc_channel = LEDC_CHANNEL_0,
+
+	.pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+	.frame_size = FRAMESIZE_VGA, //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+	.jpeg_quality = 12, //0-63 lower number means higher quality
+	.fb_count = 1, //if more than one, i2s runs in continuous mode. Use only with JPEG
+	.grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+	.fb_location = CAMERA_FB_IN_PSRAM
+};
+
+static esp_err_t init_camera(int framesize)
+{
+	ESP_LOGI(TAG, "Start init_camera");
+	ESP_LOGI(TAG, "grab_mode=%d", camera_config.grab_mode);
+	ESP_LOGI(TAG, "fb_location=%d", camera_config.fb_location);
+	//initialize the camera
+	camera_config.frame_size = framesize;
+	esp_err_t err = esp_camera_init(&camera_config);
+	if (err != ESP_OK)
+	{
+		ESP_LOGE(TAG, "Camera Init Failed");
+		return err;
+	}
+
+	ESP_LOGI(TAG, "Finish init_camera");
+	return ESP_OK;
+}
+
+static esp_err_t camera_capture(char * FileName, size_t *pictureSize)
+{
+	//clear internal queue
+	//for(int i=0;i<2;i++) {
+	for(int i=0;i<1;i++) {
+		camera_fb_t * fb = esp_camera_fb_get();
+		ESP_LOGI(TAG, "fb->len=%d", fb->len);
+		esp_camera_fb_return(fb);
+	}
+
+	//acquire a frame
+	camera_fb_t * fb = esp_camera_fb_get();
+	if (!fb) {
+		ESP_LOGE(TAG, "Camera Capture Failed");
+		return ESP_FAIL;
+	}
+
+	//replace this with your own function
+	//process_image(fb->width, fb->height, fb->format, fb->buf, fb->len);
+	FILE* f = fopen(FileName, "wb");
+	if (f == NULL) {
+		ESP_LOGE(TAG, "Failed to open file for writing");
+		return ESP_FAIL; 
+	}
+	fwrite(fb->buf, fb->len, 1, f);
+	ESP_LOGI(TAG, "fb->len=%d", fb->len);
+	*pictureSize = (size_t)fb->len;
+	fclose(f);
+	
+	//return the frame buffer back to the driver for reuse
+	esp_camera_fb_return(fb);
+
+	return ESP_OK;
+}
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			s_retry_num++;
+			ESP_LOGI(TAG, "retry to connect to the AP");
+		} else {
+			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+		}
+		ESP_LOGI(TAG,"connect to the AP fail");
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+		s_retry_num = 0;
+		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+}
+
+
+void wifi_init_sta()
+{
+	s_wifi_event_group = xEventGroupCreate();
+
+	ESP_LOGI(TAG,"ESP-IDF esp_netif");
+	ESP_ERROR_CHECK(esp_netif_init());
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_t *netif = esp_netif_create_default_wifi_sta();
+	assert(netif);
+
+#if CONFIG_STATIC_IP
+
+	ESP_LOGI(TAG, "CONFIG_STATIC_IP_ADDRESS=[%s]",CONFIG_STATIC_IP_ADDRESS);
+	ESP_LOGI(TAG, "CONFIG_STATIC_GW_ADDRESS=[%s]",CONFIG_STATIC_GW_ADDRESS);
+	ESP_LOGI(TAG, "CONFIG_STATIC_NM_ADDRESS=[%s]",CONFIG_STATIC_NM_ADDRESS);
+
+	/* Stop DHCP client */
+	ESP_ERROR_CHECK(esp_netif_dhcpc_stop(netif));
+	ESP_LOGI(TAG, "Stop DHCP Services");
+
+	/* Set STATIC IP Address */
+	esp_netif_ip_info_t ip_info;
+	memset(&ip_info, 0 , sizeof(esp_netif_ip_info_t));
+	ip_info.ip.addr = ipaddr_addr(CONFIG_STATIC_IP_ADDRESS);
+	ip_info.netmask.addr = ipaddr_addr(CONFIG_STATIC_NM_ADDRESS);
+	ip_info.gw.addr = ipaddr_addr(CONFIG_STATIC_GW_ADDRESS);;
+	esp_netif_set_ip_info(netif, &ip_info);
+
+	/*
+	I referred from here.
+	https://www.esp32.com/viewtopic.php?t=5380
+
+	if we should not be using DHCP (for example we are using static IP addresses),
+	then we need to instruct the ESP32 of the locations of the DNS servers manually.
+	Google publicly makes available two name servers with the addresses of 8.8.8.8 and 8.8.4.4.
+	*/
+
+	ip_addr_t d;
+	d.type = IPADDR_TYPE_V4;
+	d.u_addr.ip4.addr = 0x08080808; //8.8.8.8 dns
+	dns_setserver(0, &d);
+	d.u_addr.ip4.addr = 0x08080404; //8.8.4.4 dns
+	dns_setserver(1, &d);
+
+#endif // CONFIG_STATIC_IP
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+	ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = CONFIG_ESP_WIFI_SSID,
+			.password = CONFIG_ESP_WIFI_PASSWORD
+		},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+	ESP_ERROR_CHECK(esp_wifi_start() );
+
+	ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+			WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+			pdFALSE,
+			pdFALSE,
+			portMAX_DELAY);
+
+	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+	 * happened. */
+	if (bits & WIFI_CONNECTED_BIT) {
+		ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+				 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else if (bits & WIFI_FAIL_BIT) {
+		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+				 CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else {
+		ESP_LOGE(TAG, "UNEXPECTED EVENT");
+	}
+	vEventGroupDelete(s_wifi_event_group);
+}
+
+void initialize_mdns(void)
+{
+	//initialize mDNS
+	ESP_ERROR_CHECK( mdns_init() );
+	//set mDNS hostname (required if you want to advertise services)
+	ESP_ERROR_CHECK( mdns_hostname_set(CONFIG_MDNS_HOSTNAME) );
+	ESP_LOGI(TAG, "mdns hostname set to: [%s]", CONFIG_MDNS_HOSTNAME);
+
+#if 0
+	//set default mDNS instance name
+	ESP_ERROR_CHECK( mdns_instance_name_set("ESP32 with mDNS") );
+#endif
+}
+
+esp_err_t mountSPIFFS(char * partition_label, char * base_path) {
+	ESP_LOGI(TAG, "Initializing SPIFFS file system");
+
+	esp_vfs_spiffs_conf_t conf = {
+		.base_path = base_path,
+		.partition_label = partition_label,
+		.max_files = 5,
+		.format_if_mount_failed = true
+	};
+
+	// Use settings defined above to initialize and mount SPIFFS filesystem.
+	// Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+	esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+	if (ret != ESP_OK) {
+		if (ret == ESP_FAIL) {
+			ESP_LOGE(TAG, "Failed to mount or format filesystem");
+		} else if (ret == ESP_ERR_NOT_FOUND) {
+			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+		} else {
+			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+		}
+		return ret;
+	}
+
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info(partition_label, &total, &used);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+	} else {
+		ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+	}
+	ESP_LOGI(TAG, "Mount SPIFFS filesystem");
+	return ret;
+}
+
+static void printSPIFFS(char * path) {
+	DIR* dir = opendir(path);
+	assert(dir != NULL);
+	while (true) {
+		struct dirent*pe = readdir(dir);
+		if (!pe) break;
+		ESP_LOGI(__FUNCTION__,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
+	}
+	closedir(dir);
+}
+
+// Calculate the size after conversion to base64
+// http://akabanessa.blog73.fc2.com/blog-entry-83.html
+int32_t calcBase64EncodedSize(int origDataSize)
+{
+	// Number of blocks in 6-bit units (rounded up in 6-bit units)
+	int32_t numBlocks6 = ((origDataSize * 8) + 5) / 6;
+	// Number of blocks in units of 4 characters (rounded up in units of 4 characters)
+	int32_t numBlocks4 = (numBlocks6 + 3) / 4;
+	// Number of characters without line breaks
+	int32_t numNetChars = numBlocks4 * 4;
+	// Size considering line breaks every 76 characters (line breaks are "\ r \ n")
+	//return numNetChars + ((numNetChars / 76) * 2);
+	return numNetChars;
+}
+
+esp_err_t Image2Base64(char * imageFileName, size_t base64_buffer_len, uint8_t * base64_buffer)
+{
+	struct stat st;
+	if (stat(imageFileName, &st) != 0) {
+		ESP_LOGE(TAG, "[%s] not found", imageFileName);
+		return ESP_FAIL;
+	}
+	ESP_LOGI(TAG, "%s st.st_size=%ld", imageFileName, st.st_size);
+
+	// Allocate image memory
+	unsigned char*	image_buffer = NULL;
+	size_t image_buffer_len = st.st_size;
+	image_buffer = malloc(image_buffer_len);
+	if (image_buffer == NULL) {
+		ESP_LOGE(TAG, "malloc fail. image_buffer_len %d", image_buffer_len);
+		return ESP_FAIL;
+	}
+
+	// Read image file
+	FILE * fp_image = fopen(imageFileName,"rb");
+	if (fp_image == NULL) {
+		ESP_LOGE(TAG, "[%s] fopen fail.", imageFileName);
+		free(image_buffer);
+		return ESP_FAIL;
+	}
+	for (int i=0;i<st.st_size;i++) {
+		fread(&image_buffer[i], sizeof(char), 1, fp_image);
+	}
+	fclose(fp_image);
+
+	// Convert from JPEG to BASE64
+	size_t encord_len;
+	esp_err_t ret = mbedtls_base64_encode(base64_buffer, base64_buffer_len, &encord_len, image_buffer, st.st_size);
+	ESP_LOGI(TAG, "mbedtls_base64_encode=%d encord_len=%d", ret, encord_len);
+
+	free(image_buffer);
+	return ESP_OK;
+}
+
+static esp_err_t get_handler(httpd_req_t *req)
+{
+	if (req->method == HTTP_GET) {
+		ESP_LOGI(TAG, "Handshake done, the new connection was opened");
+		return ESP_OK;
+	}
+	httpd_ws_frame_t ws_pkt;
+	uint8_t *buf = NULL;
+	memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+	ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+	/* Set max_len = 0 to get the frame len */
+	esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "httpd_ws_recv_frame failed to get frame len with %d", ret);
+		return ret;
+	}
+	ESP_LOGI(TAG, "frame len is %d", ws_pkt.len);
+	if (ws_pkt.len) {
+		/* ws_pkt.len + 1 is for NULL termination as we are expecting a string */
+		buf = calloc(1, ws_pkt.len + 1);
+		if (buf == NULL) {
+			ESP_LOGE(TAG, "Failed to calloc memory for buf");
+			return ESP_ERR_NO_MEM;
+		}
+		ws_pkt.payload = buf;
+		/* Set max_len = ws_pkt.len to get the frame payload */
+		ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
+		if (ret != ESP_OK) {
+			ESP_LOGE(TAG, "httpd_ws_recv_frame failed with %d", ret);
+			free(buf);
+			return ret;
+		}
+		ESP_LOGI(TAG, "Got packet with message: %s", ws_pkt.payload);
+	}
+	ESP_LOGI(TAG, "Packet final: %d", ws_pkt.final);
+	ESP_LOGI(TAG, "Packet fragmented: %d", ws_pkt.fragmented);
+	ESP_LOGI(TAG, "Packet type: %d", ws_pkt.type);
+	ESP_LOGI(TAG, "Packet payload: %s", ws_pkt.payload);
+	free(buf);
+
+	char imageFileName[256];
+	//strcpy(imageFileName, "/spiffs/esp32.jpeg");
+	strcpy(imageFileName, "/spiffs/capture.jpeg");
+
+	// Delete local file
+	struct stat statBuf;
+	if (stat(imageFileName, &statBuf) == 0) {
+		// Delete it if it exists
+		unlink(imageFileName);
+		ESP_LOGI(TAG, "Delete Local file");
+	}
+
+#if CONFIG_ENABLE_FLASH
+	// Flash Light ON
+	gpio_set_level(CONFIG_GPIO_FLASH, 1);
+#endif
+
+	// Save Picture to Local file
+	int retryCounter = 0;
+	while(1) {
+		size_t pictureSize;
+		ret = camera_capture(imageFileName, &pictureSize);
+		ESP_LOGI(TAG, "camera_capture=%d",ret);
+		ESP_LOGI(TAG, "pictureSize=%d",pictureSize);
+		if (ret != ESP_OK) continue;
+		if (stat(imageFileName, &statBuf) == 0) {
+			ESP_LOGI(TAG, "st_size=%d", (int)statBuf.st_size);
+			if (statBuf.st_size == pictureSize) break;
+			retryCounter++;
+			ESP_LOGI(TAG, "Retry capture %d",retryCounter);
+			if (retryCounter > 10) {
+				ESP_LOGE(TAG, "Retry over for capture");
+				break;
+			}
+			vTaskDelay(1000);
+		}
+	} // end while
+
+#if CONFIG_ENABLE_FLASH
+	// Flash Light OFF
+	gpio_set_level(CONFIG_GPIO_FLASH, 0);
+#endif
+
+	// Get Image size
+	struct stat st;
+	if (stat(imageFileName, &st) != 0) {
+		ESP_LOGE(TAG, "[%s] not found", imageFileName);
+		return ESP_FAIL;
+	}
+	ESP_LOGI(TAG, "%s st.st_size=%ld", imageFileName, st.st_size);
+
+	// Get Base64 size
+	int32_t base64Size = calcBase64EncodedSize(st.st_size);
+	ESP_LOGI(TAG, "base64Size=%"PRIi32, base64Size);
+
+	// Allocate Base64 buffer
+	// You have to use calloc. It doesn't work with malloc.
+	uint8_t *base64_buffer = NULL;
+	size_t base64_buffer_len = base64Size + 1;
+	base64_buffer = calloc(1, base64_buffer_len);
+	if (base64_buffer == NULL) {
+		ESP_LOGE(TAG, "calloc fail. base64_buffer_len %d", base64_buffer_len);
+		return ESP_FAIL;
+	}
+	memset(base64_buffer, 0, base64_buffer_len);
+
+	// Convert from Image to Base64
+	//ret = Image2Base64("/spiffs/esp32.jpeg", base64_buffer_len, base64_buffer);
+	ret = Image2Base64(imageFileName, base64_buffer_len, base64_buffer);
+	ESP_LOGI(TAG, "Image2Base64=%d", ret);
+
+	// Send by WebSocket
+	ws_pkt.payload = base64_buffer;
+	ws_pkt.len = base64Size;
+	ret = httpd_ws_send_frame(req, &ws_pkt);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "httpd_ws_send_frame failed with %d", ret);
+	}
+	free(base64_buffer);
+	return ret;
+}
+
+static const httpd_uri_t ws = {
+		.uri		= "/ws",
+		.method		= HTTP_GET,
+		.handler	= get_handler,
+		.user_ctx	= NULL,
+		.is_websocket = true
+};
+
+
+static httpd_handle_t start_webserver(void)
+{
+	httpd_handle_t server = NULL;
+	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+	// Start the httpd server
+	ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+	if (httpd_start(&server, &config) == ESP_OK) {
+		// Registering the ws handler
+		ESP_LOGI(TAG, "Registering URI handlers");
+		httpd_register_uri_handler(server, &ws);
+		return server;
+	}
+
+	ESP_LOGI(TAG, "Error starting server!");
+	return NULL;
+}
+
+static esp_err_t stop_webserver(httpd_handle_t server)
+{
+	// Stop the httpd server
+	return httpd_stop(server);
+}
+
+static void disconnect_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+	httpd_handle_t* server = (httpd_handle_t*) arg;
+	if (*server) {
+		ESP_LOGI(TAG, "Stopping webserver");
+		if (stop_webserver(*server) == ESP_OK) {
+			*server = NULL;
+		} else {
+			ESP_LOGE(TAG, "Failed to stop http server");
+		}
+	}
+}
+
+static void connect_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+	httpd_handle_t* server = (httpd_handle_t*) arg;
+	if (*server == NULL) {
+		ESP_LOGI(TAG, "Starting webserver");
+		*server = start_webserver();
+	}
+}
+
+void app_main(void)
+{
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	// Initilize WiFi
+	wifi_init_sta();
+
+	// Initialize mDNS
+	initialize_mdns();
+
+	// Mount SPIFFS
+	char *partition_label = "storage";
+	char *base_path = "/spiffs";
+	ret = mountSPIFFS(partition_label, base_path);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "mountSPIFFS fail");
+		while(1) { vTaskDelay(1); }
+	}
+	printSPIFFS("/spiffs/");
+
+	// Initialize camera
+	int framesize = FRAMESIZE_VGA; // 640x480
+	ret = init_camera(framesize);
+	if (ret != ESP_OK) {
+		while(1) { vTaskDelay(1); }
+	}
+
+	static httpd_handle_t server = NULL;
+	ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
+	ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+
+	/* Start the server for the first time */
+	server = start_webserver();
+}
