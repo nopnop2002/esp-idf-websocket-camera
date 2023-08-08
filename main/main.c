f963f7a63e88fb4c156d5197ac03b97453039729
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-08-08 19:18:02 +0900

    Removed unused handles

--- main/main.c
+++ main/main.c
@@ -422,13 +422,12 @@ static esp_err_t get_handler(httpd_req_t *req)
 			free(buf);
 			return ret;
 		}
-		ESP_LOGI(TAG, "Got packet with message: %s", ws_pkt.payload);
+		ESP_LOGI(TAG, "Got packet with message: [%.*s]", ws_pkt.len, ws_pkt.payload);
+		free(buf);
 	}
 	ESP_LOGI(TAG, "Packet final: %d", ws_pkt.final);
 	ESP_LOGI(TAG, "Packet fragmented: %d", ws_pkt.fragmented);
 	ESP_LOGI(TAG, "Packet type: %d", ws_pkt.type);
-	ESP_LOGI(TAG, "Packet payload: %s", ws_pkt.payload);
-	free(buf);
 
 	char imageFileName[256];
 	//strcpy(imageFileName, "/spiffs/esp32.jpeg");
@@ -512,59 +511,31 @@ static esp_err_t get_handler(httpd_req_t *req)
 	return ret;
 }
 
-static const httpd_uri_t ws = {
-		.uri		= "/ws",
-		.method		= HTTP_GET,
-		.handler	= get_handler,
-		.user_ctx	= NULL,
-		.is_websocket = true
-};
-
-
-static httpd_handle_t start_webserver(void)
+/* Function to start the web server */
+esp_err_t start_webserver(void)
 {
 	httpd_handle_t server = NULL;
 	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
 	// Start the httpd server
 	ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
-	if (httpd_start(&server, &config) == ESP_OK) {
-		// Registering the ws handler
-		ESP_LOGI(TAG, "Registering URI handlers");
-		httpd_register_uri_handler(server, &ws);
-		return server;
+	if (httpd_start(&server, &config) != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to starting server!");
+		return ESP_FAIL;
 	}
 
-	ESP_LOGI(TAG, "Error starting server!");
-	return NULL;
-}
+	// Registering the ws handler
+	ESP_LOGI(TAG, "Registering URI handlers");
+	httpd_uri_t ws = {
+		.uri		= "/ws",
+		.method		= HTTP_GET,
+		.handler	= get_handler,
+		.user_ctx	= NULL,
+		.is_websocket = true
+	};
+	httpd_register_uri_handler(server, &ws);
 
-static esp_err_t stop_webserver(httpd_handle_t server)
-{
-	// Stop the httpd server
-	return httpd_stop(server);
-}
-
-static void disconnect_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
-{
-	httpd_handle_t* server = (httpd_handle_t*) arg;
-	if (*server) {
-		ESP_LOGI(TAG, "Stopping webserver");
-		if (stop_webserver(*server) == ESP_OK) {
-			*server = NULL;
-		} else {
-			ESP_LOGE(TAG, "Failed to stop http server");
-		}
-	}
-}
-
-static void connect_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
-{
-	httpd_handle_t* server = (httpd_handle_t*) arg;
-	if (*server == NULL) {
-		ESP_LOGI(TAG, "Starting webserver");
-		*server = start_webserver();
-	}
+	return ESP_OK;
 }
 
 void app_main(void)
@@ -600,10 +571,6 @@ void app_main(void)
 		while(1) { vTaskDelay(1); }
 	}
 
-	static httpd_handle_t server = NULL;
-	ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
-	ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
-
-	/* Start the server for the first time */
-	server = start_webserver();
+	/* Start the server */
+	start_webserver();
 }
